756e0296067449ba478408537f452ad2330cf25b
Ferdy Santoso <ferdysantoso@icloud.com>
2019-06-20 21:17:25 +0700

    Updates

---
+++ precache-manifest.a2b7f2374760791e78db640e17af6d11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612ef5abe42d4572fb5283392bd408a8",
+    "url": "/A-Simple-NIM-Finder/index.html"
+  },
+  {
+    "revision": "453900ae29e850b890e9",
+    "url": "/A-Simple-NIM-Finder/static/css/main.2763d84f.chunk.css"
+  },
+  {
+    "revision": "fd869d9e935acf042b2f",
+    "url": "/A-Simple-NIM-Finder/static/js/2.ba7274ba.chunk.js"
+  },
+  {
+    "revision": "453900ae29e850b890e9",
+    "url": "/A-Simple-NIM-Finder/static/js/main.34f26401.chunk.js"
+  },
+  {
+    "revision": "05c6778e0055a7bb4361",
+    "url": "/A-Simple-NIM-Finder/static/js/runtime~main.69aa67d5.js"
+  }
+]);
